13010333843b9dcbe82d068dc920fdae9025007f
Clement Jeulin <clement93jeulin@gmail.com>
2025-08-05 01:13:45 +0200

    Switched to Windows OS / Recreated the DB

--- app/config.py
+++ app/config.py
@@ -1,12 +1,17 @@
 import os
+from pathlib import Path
 from dotenv import load_dotenv
 
-load_dotenv()
+# Charge .env depuis la racine du projet, peu importe où est lancé le script
+BASE_DIR = Path(__file__).resolve().parent.parent  # = epic_events/
+load_dotenv(dotenv_path=BASE_DIR / ".env")
 
 DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_HOST = os.getenv("DB_HOST")
 DB_PORT = os.getenv("DB_PORT")
 DB_NAME = os.getenv("DB_NAME")
+
+print(f"DB_NAME={DB_NAME}, DB_USER={DB_USER}, DB_PASSWORD={DB_PASSWORD}, DB_HOST={DB_HOST}, DB_PORT={DB_PORT}")
 
 DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
