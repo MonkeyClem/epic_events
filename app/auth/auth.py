15787530f520fa500aedf0a0c5103d8e6ba6b59d
Clement Jeulin <clement93jeulin@gmail.com>
2025-08-08 19:00:43 +0200

    Added docstring in auth.py

--- app/auth/auth.py
+++ app/auth/auth.py
@@ -13,11 +13,30 @@ JWT_EXPIRATION_MINUTES = 30
 # -------------------- PASSWORD HASH----------
 
 def hash_password(plain_password: str) -> str:
+    """
+    Hash un mot de passe en texte clair à l'aide de bcrypt.
+
+    Args:
+        plain_password (str): Le mot de passe en texte clair à hasher.
+
+    Returns:
+        str: Le mot de passe hashé sous forme de chaîne de caractères.
+    """
     return bcrypt.hashpw(plain_password.encode("utf-8"), bcrypt.gensalt()).decode(
         "utf-8"
     )
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
+    """
+    Vérifie si le mot de passe en texte clair correspond au mot de passe hashé.
+
+    Args:
+        plain_password (str): Le mot de passe en texte clair à vérifier.
+        hashed_password (str): Le mot de passe hashé à comparer.
+
+    Returns:
+        bool: True si le mot de passe en texte clair correspond au hash, sinon False.
+    """
     return bcrypt.checkpw(
         plain_password.encode("utf-8"), hashed_password.encode("utf-8")
     )
@@ -27,6 +46,15 @@ def verify_password(plain_password: str, hashed_password: str) -> bool:
 
 
 def create_token(user_id: int) -> str:
+    """
+    Crée un token JWT pour un utilisateur donné.
+
+    Args:
+        user_id (int): L'ID de l'utilisateur pour lequel générer le token.
+
+    Returns:
+        str: Le token JWT généré.
+    """
     payload = {
         "user_id": user_id,
         "exp": datetime.now(timezone.utc) + timedelta(minutes=JWT_EXPIRATION_MINUTES),
@@ -36,6 +64,15 @@ def create_token(user_id: int) -> str:
 
 
 def decode_token(token: str):
+    """
+    Décode un token JWT et retourne l'ID de l'utilisateur.
+
+    Args:
+        token (str): Le token JWT à décoder.
+
+    Returns:
+        int | None: L'ID de l'utilisateur si le token est valide, sinon None.
+    """
     try:
         payload = jwt.decode(token, JWT_SECRET, algorithms=[JWT_ALGORITHM])
         return payload["user_id"]
@@ -46,6 +83,19 @@ def decode_token(token: str):
 
 
 def verify_token(token: str) -> dict:
+    """
+    Vérifie la validité d'un token JWT. Si valide, retourne l'ID de l'utilisateur.
+
+    Args:
+        token (str): Le token JWT à vérifier.
+
+    Raises:
+        jwt.ExpiredSignatureError: Si le token a expiré.
+        jwt.InvalidTokenError: Si le token est invalide.
+
+    Returns:
+        int: L'ID de l'utilisateur si le token est valide.
+    """
     try:
         payload = jwt.decode(token, JWT_SECRET, algorithms=[JWT_ALGORITHM])
         return payload["user_id"]
